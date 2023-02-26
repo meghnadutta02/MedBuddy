7ad2157eeb6ce0b0bae0fe7e1142ac8dd7fb65fd
Meghna Dutta <meghnakha18@gmail.com>
2023-02-26 05:59:19 +0530

    appointment booked alert added

--- frontend/src/pages/AppointmentComponent.js
+++ frontend/src/pages/AppointmentComponent.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Row,Col } from 'react-bootstrap';
-export default function AppointmentComponent({index, doctor, specialty, date, time,Click }) {
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Row, Col } from "react-bootstrap";
+import Alert from 'react-bootstrap/Alert';
+export default function AppointmentComponent({
+  index,
+  doctor,
+  specialty,
+  date,
+  time,
+  image,
+  Click,
+}) {
+  const [onclick, setClicked] = useState(false);
+  function Click() {
+    setClicked(true);
+  }
   return (
-
-        <Card key={index} style={{ marginTop: "30px", marginBottom: "50px" }}>
-          <Row className='justify-content-center'>
-            <Col lg={3}>
-              <Card.Img
-                fluid
-                crossOrigin="anonymous"
-                variant="top"
-                src={"/images/doctor.jpg"}
-              />
-            </Col>
-            <Col lg={7}>
-              <Card.Body>
-                <Card.Title>{doctor}</Card.Title>
-                <Card.Text>{specialty}</Card.Text>
-                <Card.Text className="h4">
-                  {date}, {time}
-                </Card.Text>
-                <Button onClick={()=>Click()} variant="primary">Book Appointment</Button>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card>
+    <Card key={index} className="pb-3" style={{ marginTop: "30px", marginBottom: "50px" }}>
+      <Row className="justify-content-center">
+        <Col lg={3} sm={4}>
+          <Card.Img
+            fluid
+            crossOrigin="anonymous"
+            variant="top"
+            src={image}
+          />
+        </Col>
+        <Col lg={7}>
+          <Card.Body>
+            <Card.Title>{doctor}</Card.Title>
+            <Card.Text>{specialty}</Card.Text>
+            <Card.Text className="h4">
+              {date}, {time}
+            </Card.Text>
+            {onclick === false && (
+              <Button onClick={Click} variant="primary">
+                Book Appointment
+              </Button>
+            )}
+            {onclick && (
+              <>
+              <Button onClick={Click} variant="primary" disabled>Booked </Button>
+              <Alert className="mt-2" key={"success"} variant={"success"}>Your Appointment is booked!</Alert>
+              </>
+            )}
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
   );
 }
--- frontend/src/pages/Appointments.js
+++ frontend/src/pages/Appointments.js
@@ -3,12 +3,12 @@ import AppointmentComponent from './AppointmentComponent';
 
 export default function Appointments() {
     const appointments = [
-        { doctor: 'Dr. Smith', specialty: 'Cardiology', date: 'March 1st, 2023', time: '10:00 AM' },
-        { doctor: 'Dr. Johnson', specialty: 'Dermatology', date: 'March 3rd, 2023', time: '2:00 PM' },
-        { doctor: 'Dr. Lee', specialty: 'Oncology', date: 'March 5th, 2023', time: '11:00 AM' },
-        { doctor: 'Dr. Kim', specialty: 'Gynecology', date: 'March 8th, 2023', time: '3:00 PM' },
-        { doctor: 'Dr. Brown', specialty: 'Orthopedics', date: 'March 10th, 2023', time: '9:00 AM' },
-        { doctor: 'Dr. Chen', specialty: 'Neurology', date: 'March 12th, 2023', time: '12:00 PM' },
+        { doctor: 'Dr. Smith', specialty: 'Cardiology', date: 'March 1st, 2023', time: '10:00 AM',image:"/images/doctor.jpg" },
+        { doctor: 'Dr. Johnson', specialty: 'Dermatology', date: 'March 3rd, 2023', time: '2:00 PM',image:"/images/doctor1.jpg" },
+        { doctor: 'Dr. Lee', specialty: 'Oncology', date: 'March 5th, 2023', time: '11:00 AM',image:"/images/doctor.jpg" },
+        { doctor: 'Dr. Kim', specialty: 'Gynecology', date: 'March 8th, 2023', time: '3:00 PM' ,image:"/images/doctor1.jpg"},
+        { doctor: 'Dr. Brown', specialty: 'Orthopedics', date: 'March 10th, 2023', time: '9:00 AM',image:"/images/doctor.jpg" },
+        { doctor: 'Dr. Chen', specialty: 'Neurology', date: 'March 12th, 2023', time: '12:00 PM',image:"/images/doctor.jpg" },
       ];
   const handleBookAppointment = (appointment) => {
     console.log(`Booked appointment with ${appointment.doctor} on ${appointment.date} at ${appointment.time}`);
@@ -24,6 +24,7 @@ export default function Appointments() {
           specialty={appointment.specialty}
           date={appointment.date}
           time={appointment.time}
+          image={appointment.image}
           Click={handleBookAppointment(appointment)}
         />
       ))}
